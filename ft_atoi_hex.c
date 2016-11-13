fe9548a39fa39524816b9b1bf08467aa912c7dbb
Baptiste BICHERON <bbichero@e1r6p1.42.fr>
2016-11-13 15:35:51 +0100

    Show alloc mem done

---
+++ ft_atoi_hex.c
@@ -0,0 +1,22 @@
+#include "malloc.h"
+
+void	ft_atoi_hex(void *ptr)
+{
+    char				*base;
+    unsigned long long	address;
+    char				hex[10];
+    int					i;
+
+    base = "0123456789abcdef";
+    address = (unsigned long long)ptr;
+    ft_bzero(hex, 10);
+    i = 8;
+    while (address)
+    {
+        hex[i] = base[address % 16];
+        address = address / 16;
+        i--;
+    }
+    ft_putstr("0x");
+    ft_putstr(hex);
+}
